598c38618bb034f55b9a0633a5e3d351f692c6ca
Crispin Wellington <retrogradeorbit@gmail.com>
2015-08-08 10:36:26 +0800

    reading a maildir and counting the unread messages

--- src/uplink/core.clj
+++ src/uplink/core.clj
@@ -1,7 +1,30 @@
 (ns uplink.core
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+
+(comment
+
+  (def md-cur
+    (-> "user.home"
+        System/getProperty
+        (str "/Maildir")
+        io/file
+        (io/file "cur")))
+  (.lastModified md-cur)
+  (-> md-cur .listFiles count)
+  (-> md-cur .listFiles (aget 2) .getName (string/split #":"))
+
+
+  (def mdir (io/file maildir))
+
+
+
+
+  )
